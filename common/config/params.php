602bfccb216dd8b742f32e61d969a14e7e8188ab
Hui Liang <lianghui2@100tal.com>
2019-05-15 11:02:33 +0800

    fix gitignore

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,10 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => '1173240549@qq.com',
+    'supportEmail' => '2261818969@qq.com',
     'user.passwordResetTokenExpire' => 3600,
+    'backendUrl' => 'https://back.jumoshen.cn/',
+    'frontendUrl' => 'https://www.jumoshen.cn/',
+    'socket' => 'ws://23.83.251.75:9501',
+    'webMenu' => \backend\models\StudyCourse::allCourseType(true),
 ];
