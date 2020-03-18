2cacffaad9e5157d5258efca3ed23eb9597830bc
jumoshen <1173240549@qq.com>
2020-03-18 23:42:32 +0800

    add live2d&change source`s https protocol

--- frontend/views/study-course/view.php
+++ frontend/views/study-course/view.php
@@ -90,7 +90,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 var conf = 'prod_870022d974db54a56fa85b8635e90ab9';
                 var width = window.innerWidth || document.documentElement.clientWidth;
                 if (width < 960) {
-                    window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
+                    window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
                 } else {
                     var loadJs = function (d, a) {
                         var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
@@ -113,7 +113,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         }
                         c.appendChild(b)
                     };
-                    loadJs("http://changyan.sohu.com/upload/changyan.js", function () {
+                    loadJs("https://changyan.sohu.com/upload/changyan.js", function () {
                         window.changyan.api.config({appid: appid, conf: conf})
                     });
                 }
